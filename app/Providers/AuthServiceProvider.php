c8e4a88dcd603c159c7b05dd082de45967fadff4
Pavel Kutáč <pavel.kutac@gmail.com>
2021-03-11 18:44:22 +0100

    Revert "Bug no.19 - Admin nemá přístup v adminu do žádné záložky"
    This reverts commit 12b5e9669edd27e3661219a23a8e67e8f5a94d7c.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -44,7 +44,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('access-admin-routes', static function (User $user) {
-            return $user->isRoleMaster();
+            return $user->isAdminOrMore();
         });
 
         Gate::resource('exports', ExportPolicy::class, [
